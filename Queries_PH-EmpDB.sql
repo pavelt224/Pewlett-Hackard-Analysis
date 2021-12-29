db73d4359a2cc77dc426b32502a8408f8e2804a5
Pavel Tenenbaum <93852380+pavelt224@users.noreply.github.com>
2021-12-29 01:00:08 -0500

    Up4

--- Queries_PH-EmpDB.sql
+++ Queries_PH-EmpDB.sql
@@ -21,3 +21,42 @@ rt.title
 INTO unique_titles
 FROM retirement_titles AS rt
 ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- creating retiring_titles
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY Count DESC; 
+
+
+-- End of Deliverable 1
+
+-- Start of Deliverable 2
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+--INTO nameyourtable
+--FROM _______
+--ORDER BY _____, _____ DESC;
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+    ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+	ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+    ON (e.emp_no = ti.emp_no)
+-- filter on to_date to get current employees ('999-01-01')
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
